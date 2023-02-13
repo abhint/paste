93b8128dd10adc6e523d4ef9f19991959282c1ba
abhint <notifyabhijith@gmail.com>
2023-02-13 22:21:12 +0530

    to show notes

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from flask_restful import Api
-from until import APIHandling
+from until import APIHandling, View
 from flask import Flask, render_template
 
 app = Flask(__name__)
 api = Api(app=app)
 
-
 api.add_resource(APIHandling, '/api')
+api.add_resource(View, '/api/p/<key>')
+
+
+@app.route('/<key>')
+def view_(key: str):
+    print(key)
+    return render_template("index.html")
 
 
 @app.route('/')
