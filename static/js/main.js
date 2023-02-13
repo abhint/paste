8dec6c081250c47e291bd7e9f665496bcaa40749
abhint <notifyabhijith@gmail.com>
2023-02-13 22:15:37 +0530

    to show notes

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,27 @@
-// const var_re = document.getElementById("re");
-// var_re.addEventListener("click", (e) => {
-//   fetch("/api").then((res) => console.log(res));
-// });
-
 class Paste {
   constructor() {
-    this.btn = document.querySelector(".btn");
+    this.btn = document.getElementById("save");
+    this.urlText = document.querySelector(".key");
     this.editor = CodeMirror(document.getElementById("textArea"), {
       placeholder: "Paste code, save and share the link!",
       lineNumbers: true,
       theme: "darcula",
+    });
+  }
+
+  async copy(url) {
+    // https://stackoverflow.com/a/45071478/11913751
+
+    this.urlText.onclick = function () {
+      document.execCommand("copy");
+    };
+
+    this.urlText.addEventListener("copy", function (event) {
+      event.preventDefault();
+      if (event.clipboardData) {
+        event.clipboardData.setData("text/plain", url);
+        console.log(event.clipboardData.getData("text"));
+      }
     });
   }
 
@@ -29,50 +41,29 @@ class Paste {
       this.request(content);
     });
   }
+
+  async load() {
+    let urlPath = window.location.pathname;
+    if (urlPath === "/") return;
+    let response = await fetch(`/api/p/${urlPath}`, {
+      method: "POST",
+    });
+
+    if (!response.ok) return (window.location = "/");
+    this.btn.style.visibility = "hidden";
+    let result = await response.json();
+
+    this.editor.getDoc().setValue(result.content);
+    this.editor.setOption("readOnly", true);
+
+    this.urlText.innerHTML = `/${result.key}`;
+    await this.copy(`${window.location.origin}/${result.key}`);
+  }
 }
 
 window.addEventListener("DOMContentLoaded", async (_) => {
   codeEditor = new CodeMirror();
   const past = new Paste();
   past.save();
+  past.load();
 });
-
-// var lineCountCache = 0;
-// const lineNumber = document.getElementById("lineCounter");
-// const userTextarea = document.getElementById("userTextarea");
-
-// const lengthOftextarea = (text) => {
-//   return text.length ? text.split(/\r\n|\r|\n/).length : 0;
-// };
-
-// const lineCounter = (lineCount) => {
-//   var lineCountList = [];
-//   if (lineCountCache == lineCount) return;
-//   for (var number = 0; number < lineCount; number++) {
-//     lineCountList.push(`${number + 1}. `);
-//   }
-//   lineNumber.textContent = lineCountList.join("\n");
-// };
-// userTextarea.addEventListener("input", userTextCount);
-// function userTextCount(e) {
-//   let text = e.target.value;
-//   let lineCount = lengthOftextarea(text);
-
-//   lineCounter(lineCount);
-// }
-
-// userTextarea.addEventListener("keydown", (e) => {
-//   let keyCode = e.keyCode;
-//   switch (keyCode) {
-//     case 8:
-//       let textlength = userTextarea.value.length;
-//       console.log(textlength);
-
-//       if (textlength != 0) {
-//         lineNumber.textContent = " ";
-//       }
-//       break;
-//     default:
-//       return;
-//   }
-// });
