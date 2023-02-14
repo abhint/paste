001d7def88355846eb3a19ebc9d1ab707599331a
abhint <notifyabhijith@gmail.com>
2023-02-14 13:24:09 +0530

    Optional

--- until/err.py
+++ until/err.py
@@ -1,10 +1,15 @@
+from typing import Optional
 from werkzeug.exceptions import HTTPException
 
 
 class Error(HTTPException):
+    code: Optional[int] = None
+    description: Optional[str] = None
     err400 = "Item Not found"
 
-    def __init__(self, code, description):
+    def __init__(self,
+                 code: Optional[int],
+                 description: Optional[str]):
         self.code = code
         self.description = description
         super().__init__(self.code, self.description)
