a59ce9887fc85c973937e8582347c460e3e6bcb6
abhint <notifyabhijith@gmail.com>
2023-02-23 11:24:01 +0530

    about

--- static/js/about.js
+++ static/js/about.js
@@ -1,3 +1,11 @@
 document.getElementById("about").innerHTML = marked.parse(
-    "# Paste \n`shell echo`"
+  "# Paste \n \
+    The paste is an open-source and free web application. \
+    The Past allows users to upload and share text content online.\n \
+    ## Features\n \
+    - Lightweight Web Application \
+    - Paste your content anonymously. \
+    - Content can be saved using the API. \
+    - Easy to use as it has a simple interface.\
+    - One-click URL copy."
 );
